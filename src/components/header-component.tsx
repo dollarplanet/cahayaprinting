74d1cc93a1ab28044928d38139ff1ce8c42aa675
dollarplanet <agusriyanto.artist@gmail.com>
2024-12-22 20:32:16 +0700

    header fix

--- src/components/header-component.tsx
+++ src/components/header-component.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 export const HeaderComponent = (props: Props) => {
   return (
-    <header className="sticky backdrop-blur inset-x-0 top-0 z-10 w-full bg-orange-300/50">
+    <header className="backdrop-blur inset-x-0 top-0 z-10 w-full bg-orange-300/50">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 lg:h-20">
           <div className="flex-shrink-0">
