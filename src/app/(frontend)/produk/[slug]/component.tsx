9b5c8b2c8e703b64c58c809a3b9c455e04f035a9
dollarplanet <agusriyanto.artist@gmail.com>
2024-12-31 11:40:01 +0700

    product variant color

--- src/app/(frontend)/produk/[slug]/component.tsx
+++ src/app/(frontend)/produk/[slug]/component.tsx
@@ -106,8 +106,8 @@ export const ProductComponent = (props: Props) => {
                 }
 
                 return (<div key={index}>
-                  <label htmlFor={variant.id.toString()} className="block text-sm font-medium text-gray-900 dark:text-white">{variant.name}</label>
-                  <select {...priceVariantForm.register(option.variant.toString())} defaultValue={option.options[0].value} id={variant.id.toString()} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <label htmlFor={variant.id.toString()} className="block text-sm font-medium text-gray-900">{variant.name}</label>
+                  <select {...priceVariantForm.register(option.variant.toString())} defaultValue={option.options[0].value} id={variant.id.toString()} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                     {option.options.map((opt, index) =>
                       <option value={opt.value} key={index}>{opt.label}</option>
                     )}
@@ -123,8 +123,8 @@ export const ProductComponent = (props: Props) => {
                 }
 
                 return (<div key={index}>
-                  <label htmlFor={variant.id.toString()} className="block text-sm font-medium text-gray-900 dark:text-white">{variant.name}</label>
-                  <select {...freeVariantForm.register(option.variant.toString())} defaultValue={option.options[0].value} id={variant.id.toString()} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                  <label htmlFor={variant.id.toString()} className="block text-sm font-medium text-gray-900">{variant.name}</label>
+                  <select {...freeVariantForm.register(option.variant.toString())} defaultValue={option.options[0].value} id={variant.id.toString()} className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                     {option.options.map((opt, index) =>
                       <option value={opt.value} key={index}>{opt.label}</option>
                     )}
